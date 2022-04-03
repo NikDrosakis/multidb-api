7158d09eebf37c3ec42a478c87c7fdf2bd3f08ab
Nik Drosakis <nikosdrosakis@gmail.com>
2022-04-03 22:40:18 +0300

    Delete php directory

--- php/P/appregistration.php
+++
@@ -1,8 +0,0 @@
-<?php
-$user= $spd->fetch("SELECT uid,loggedin FROM user_app WHERE regid=?",array($this->id));
-if(empty($user)){
-//    $q= $spd->query("INSERT INTO user_app (regid,loggedin) VALUES(?,1)", array($this->id));
-    $q= $spd->query("INSERT INTO user_app (regid) VALUES(?)", array($this->id));
-}
-    $data='ok';
-
